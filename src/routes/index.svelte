5a7c6e5185a2342e28f99847493298d5749ab32b
Arthur Helfstein Fragoso <arthur@life.net.br>
2022-01-23 22:21:32 -0300

    Adds Onboard example

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,53 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang='ts'>
+import Onboard from 'bnc-onboard';
+import type { API } from 'bnc-onboard/dist/src/interfaces';
+import { onMount } from 'svelte';
+
+let onboard: API;
+
+onMount(()=>{
+    onboard = Onboard({
+      networkId: 1,
+      walletSelect: {
+        wallets: [
+          { walletName: 'metamask' },
+          { walletName: "coinbase" },
+        ]
+      },
+    })
+})
+
+const selectAndConnect = async () => {
+    if (!onboard) return;
+
+    await onboard.walletSelect();
+    await onboard.walletCheck();
+}
+
+</script>
+
+
+<main>
+
+<h1>Testing Onboard on SvelteKit</h1>
+
+<button on:click={selectAndConnect}>Select Wallet and Connect</button>
+
+
+</main>
+
+<style>
+
+main {
+    text-align: center;
+    margin-top: 20vh;
+}
+
+button {
+    padding: 1rem;
+    background: #eef9ec;
+    border-radius: 1rem;
+    border: 2px solid #8cbef2;
+}
+
+</style>
